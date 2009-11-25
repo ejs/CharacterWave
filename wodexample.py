6d0ed833fd9373684c1d39d968397711d04347b7
Edward Saxton <eddsax@googlemail.com>
2009-11-25 17:10:55 +0000

    Start building a CLI nWoD front end.

---
+++ wodexample.py
@@ -0,0 +1,23 @@
+import ideas
+
+
+def filter(self, rep, key):
+    if key == "defence":
+        return min(self['dex'], self['wits'])
+    if type(rep) == str:
+        try:
+            return sum((self[i] if i in self else int(i)) for i in rep.split())
+        except Exception, e:
+            return rep
+    else:
+        return rep
+
+
+if __name__ == '__main__':
+    import sys
+
+    defaults = ideas.Character(open('changeling_defaults'))
+    world = ideas.World(defaults, [filter])
+    world.load(open('mel.txt'))
+    a = sys.argv[1]
+    print sum(world['mel'][i] for i in a.split())

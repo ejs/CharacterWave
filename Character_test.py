af8304199b24f42b404187fd6725170ee1d9eb5f
Edward Saxton <eddsax@googlemail.com>
2009-11-25 01:57:29 +0000

    Move to more complicated model of a character.

--- Character_test.py
+++ Character_test.py
@@ -1,14 +1,77 @@
 import ideas
 
 
-def test_load():
-    char = ideas.load(open('mel.txt'))
-    print char
+defaults = {'academics': -3, 'computer': -3, 'init':'dex comp'}
+
+
+def filter(self, rep):
+    if type(rep) == str:
+        items = rep.split()
+        if all(i in self for i in items):
+            return sum(self[s] for s in items)
+        else:
+            return rep
+    else:
+        return rep
+
+
+def test_character():
+    char = ideas.Character(open('mel.txt'))
     assert len(char['name']) == 3
     assert 'Mel' in char['name']
     assert char['Int'] == 2
+    assert char['int'] == 2
     assert char['Dex'] == 3
     assert char['Man'] == 2
     assert char['Academics'] == 1
     assert char['Craft'] == 1
+    assert char['attack'] == 'dex firearms pistol'
     assert char['Monky'] == 0
+
+
+def test_world():
+    world = ideas.World()
+    world.load(open('mel.txt'))
+    assert len(world) == 1
+    assert 'Mel' in world
+    assert 'Melba Brandon' in world
+    assert world['Mel'] is world['Melba Brandon']
+    char = world['Mel']
+    assert 'Mel' in char['name']
+    assert char['Int'] == 2
+
+
+def test_defaults():
+    char = ideas.Character(open('mel.txt'), defaults)
+    assert char['int'] == 2
+    assert char['computer'] == -3
+    assert char['init'] == 'dex comp'
+
+
+def test_world_defaults():
+    world = ideas.World(defaults)
+    world.load(open('mel.txt'))
+    char = world['Mel']
+    assert char['int'] == 2
+    assert char['computer'] == -3
+    assert char['init'] == 'dex comp'
+
+
+def test_filter():
+    filters = [filter]
+    char = ideas.Character(open('mel.txt'), defaults, filters)
+    assert char['computer'] == -3
+    assert char['int'] == 2
+    assert char['attack'] == 6
+    assert char['init'] == 6
+
+
+def test_world_filters():
+    filters = [filter]
+    world = ideas.World(defaults, filters)
+    world.load(open('mel.txt'))
+    char = world['Mel']
+    assert char['int'] == 2
+    assert char['attack'] == 6
+    assert char['computer'] == -3
+    assert char['init'] == 6
--- ideas.py
+++ ideas.py
@@ -1,25 +1,92 @@
 from collections import defaultdict
+from UserDict import DictMixin
 import re
 
+class World(object):
+    def __init__(self, defaults=None, filters=None):
+        self.store = {}
+        self.defaults = defaults or {}
+        self.filters = filters or []
 
-expressions = {}
-expressions['name'] = re.compile("^(?:name\s?:?)\s*([\w\s]*)$", re.I)
-expressions['stat'] = re.compile("^[\s\*\#]*([\w]+)[\s\:]+(\d+)", re.I)
+    def __len__(self):
+        seen = []
+        for i in self.store.values():
+            if i not in seen:
+                seen.append(i)
+        return len(seen)
+
+    def __getitem__(self, key):
+        return self.store[key]
+    
+    def __contains__(self, item):
+        return item in self.store
+
+    def load(self, source):
+        char = Character(source, self.defaults, self.filters)
+        for n in char['name']:
+            self.store[n] = char
 
 
-def load(source):
-    character = defaultdict(int)
-    for line in source:
-        i = expressions['name'].match(line)
+class Character(DictMixin):
+    expressions = {}
+
+    def __init__(self, source, defaults=None, filters=None):
+        self.filters = filters or []
+        self.store = {}
+        self.defaults = defaults or {}
+        methods = (self._catch_blank, self._catch_name, self._catch_int_stat, self._catch_stat)
+        for line in source:
+            if line.strip():
+                for m in methods:
+                    if m(line):
+                        break
+                else:
+                    print line.strip()
+
+    def _catch_blank(self, line):
+        return not line.strip()
+
+    def _catch_name(self, line):
+        if 'name' not in self.expressions:
+            self.expressions['name'] = re.compile("^(?:name\s?)[\s:]*([\w\s]+)$", re.I)
+        i = self.expressions['name'].match(line)
         if i:
-            if 'name' in character:
-                character['name'].append(i.group(1).strip())
-            else:
-                character['name'] = [i.group(1).strip()]
-            continue
-        i = expressions['stat'].match(line)
-        print i
-        if i:
-            character[i.group(1).strip()] = int(i.group(2).strip())
-    return character
+            self.store.setdefault('name', []).append(i.group(1).strip())
+        return i
 
+    def _catch_int_stat(self, line):
+        if 'intstat' not in self.expressions:
+            self.expressions['intstat'] = re.compile("^[\s\*\#]*([\w\s\(\)]+)\s*:*\s*(\d+)", re.I)
+        i = self.expressions['intstat'].match(line)
+        if i:
+            self[i.group(1)] = int(i.group(2).strip())
+        return i
+
+    def __contains__(self, item):
+        return item in self.store or item in self.defaults
+
+    def _catch_stat(self, line):
+        if 'stat' not in self.expressions:
+            self.expressions['stat'] = re.compile("^[\s\*\#]*([\w\s\(\)]+)\s*:*\s*([\d\s\w\(\)]+)", re.I)
+        i = self.expressions['stat'].match(line)
+        if i:
+            self[i.group(1)] = i.group(2).strip()
+        return i
+    
+    def __setitem__(self, key, value):
+        self.store[key.strip().lower()] = value
+
+    def __delitem__(self, key):
+        del self.store[key]
+
+    def __getitem__(self, key):
+        key = key.lower().strip()
+        if key in self.store:
+            value = self.store[key]
+        elif key in self.defaults:
+            value = self.defaults[key]
+        else:
+            return 0
+        for f in self.filters:
+            value = f(self, value)
+        return value
